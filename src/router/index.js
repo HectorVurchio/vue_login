1cbeff1b4ca88d89fbfc09b419a408a80b1c3b91
Hector Vurchio <hectorvurchio@gmail.com>
2023-04-29 14:22:13 -0400

    vue 3 login

---
+++ src/router/index.js
@@ -0,0 +1,45 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import Dashboard from "../views/DashboardView.vue";
+import RegisterUser from "../views/RegisterUser.vue";
+import LoginUser from "../views/LoginView.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/dashboard",
+    name: "dashboard",
+    component: Dashboard,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: RegisterUser,
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: LoginUser,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const loggedIn = localStorage.getItem("user");
+
+  if (to.matched.some((record) => record.meta.requiresAuth) && !loggedIn) {
+    next("/");
+  }
+  next();
+});
+
+export default router;
